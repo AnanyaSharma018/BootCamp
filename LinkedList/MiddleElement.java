c8eabe6bfc7ebbf41479615b38c27a12fc43eb83
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-06 22:37:49 +0530

    Add files via upload

---
+++ LinkedList/MiddleElement.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class MiddleElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        Node head = null, tail = null;
+
+        System.out.print("Enter elements: ");
+        for (int i = 0; i < n; i++) {
+            Node newNode = new Node(sc.nextInt());
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println("Middle element = " + slow.data);
+
+        sc.close();
+    }
+}
