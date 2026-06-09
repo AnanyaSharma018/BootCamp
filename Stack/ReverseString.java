7d9b8d4f214a27c2faf3d5280665db6b0d634ccd
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-10 01:10:52 +0530

    Add files via upload

---
+++ Stack/ReverseString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+
+        
+        System.out.print("Reversed String: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+
+        sc.close();
+    }
+}
